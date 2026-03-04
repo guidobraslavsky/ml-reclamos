74dbca71aa857fc4a9a7c5bb6fa369b56681bc60
Guido Braslavsky <braslavskyguido@gmail.com>
2026-03-04 10:54:02 -0300

    usuario;contraseña;admin.html

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -8,13 +8,18 @@ admin_bp = Blueprint("admin", __name__)
 @admin_bp.route("/admin/login", methods=["GET", "POST"])
 def admin_login():
     if request.method == "POST":
+
+        username = request.form.get("username")
         password = request.form.get("password")
 
-        if password == admin_password:
+        admin_user = os.environ.get("ADMIN_USER")
+        admin_password = os.environ.get("ADMIN_PASSWORD")
+
+        if username == admin_user and password == admin_password:
             session["admin_logged"] = True
             return redirect("/admin")
 
-    return render_template("login.html")
+    return render_template("login.html", error=True)
 
 
 @admin_bp.route("/admin/logout")
